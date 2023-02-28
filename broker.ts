f0a56975a0c59aa7d248c061e3c6a17ed2c81a14
Grapeka <chotwilaiwan@gmail.com>
2023-02-28 22:45:23 +0700

    `style` : add comment

--- broker.ts
+++ broker.ts
@@ -4,48 +4,65 @@ const chalk = require('chalk');
 const ipaddr = require('ipaddr.js');
 const dotenv = require('dotenv');
 
+// Load environment variables from .env file
 dotenv.config();
 
+// Define port to listen on
 const PORT = process.env.PORT || 1883;
 
+// Create an Aedes instance
 const app = new aedes();
+// Create an MQTT server
 const server = createServer(app);
 
 let chunk: any = {};
 
+// Listen for 'client' and 'clientDisconnect' events emitted by the server
 server.listen(PORT, () => {
+  // Log the port the server is listening on
   console.log(`Server listening on port ${PORT}\n`);
 
-  app.on('client', (client) => {
+  // Listen for 'client' events emitted by the server
+  app.on('client', (client: any) => {
     //@ts-ignore
     let clientIp = ipaddr.parse(client.conn._sockname.address);
 
+    // Log the client ID and IP address
     console.log(
       chalk.bgGreen.bold('Client connected:'),
       `${client.id} from ${clientIp.toString()} ${clientIp.kind()}`
     );
   });
 
-  app.on('clientDisconnect', (client) => {
+  // Listen for 'clientDisconnect' events emitted by the server
+  app.on('clientDisconnect', (client: any) => {
     //@ts-ignore
     let clientIp = ipaddr.parse(client.conn._sockname.address);
 
+    // Log the client ID and IP address
     console.log(
       chalk.bgRed.bold('Client disconnected:'),
       `${client.id} from ${clientIp.toString()} ${clientIp.kind()}`
     );
   });
 
-  app.on('publish', (packet, client) => {
+  // Listen for 'publish' events emitted by the server
+  app.on('publish', (packet: any, client: any) => {
+    // If the client ID is not defined, return
     if (!client?.id) {
       return;
     }
+    // If the chunk is not defined, define it
     if (!chunk[client.id]) chunk[client.id] = [];
+
+    // If the packet topic is 'sensorData/end', publish the sensor data to the 'sensorData/final' topic
     if (packet.topic === 'sensorData/end') {
       console.log('Received sensor data from', client?.id);
 
       const allData = chunk[client.id].join('');
+      console.log('All data: ', allData);
 
+      // Publish the sensor data to the 'sensorData/final' topic
       app.publish(
         {
           topic: 'sensorData/final',
@@ -65,8 +82,11 @@ server.listen(PORT, () => {
           console.log('Send sensor data to server');
         }
       );
+      // If the packet topic is 'sensorData/start', define the chunk
     } else if (packet.topic === 'sensorData/start') {
       chunk[client.id] = [];
+
+      // If the packet topic starts with 'sensorData', add the payload to the chunk
     } else if (packet.topic.startsWith('sensorData')) {
       const part = packet.topic.split('/')[1];
       chunk[client.id][part] = packet.payload;
--- server.ts
+++ server.ts
@@ -5,6 +5,7 @@ const sqlite3 = require('better-sqlite3');
 let dbDir: string = '';
 let count: number = 1;
 
+// Check if the user has specified a database file
 if (process.argv[2]) {
   dbDir = process.argv[2];
 } else {
@@ -13,8 +14,11 @@ if (process.argv[2]) {
   }
   dbDir = `./db/mqtt${count}.sqlite`;
 }
+
+// Create a database connection
 const db = sqlite3(dbDir);
 
+// Create a table to store sensor data
 db.prepare(
   `
   CREATE TABLE IF NOT EXISTS sensorData (
@@ -29,15 +33,22 @@ db.prepare(
 `
 ).run();
 
+// Connect to the MQTT broker
 const client = mqtt.connect('mqtt://localhost:1883');
 
+// Listen for 'connect' events emitted by the client
 client.on('connect', () => {
   console.log('Connected to MQTT server at localhost:1883 writing at ', dbDir);
 
+  // Subscribe to the 'sensorData/final' topic
   client.subscribe('sensorData/final');
 
-  client.on('message', (topic, message) => {
+  // Listen for 'message' events emitted by the client
+  client.on('message', (topic: string, message: Buffer) => {
     const data = JSON.parse(message.toString());
+    console.log('Data from broker: ', data);
+
+    // Insert the sensor data into the database
     db.prepare(
       `
         INSERT INTO sensorData (timestamp, clientID, ipAddr, humidity, temperature, thermalArray
@@ -52,7 +63,7 @@ client.on('connect', () => {
       data.temperature,
       JSON.stringify(data.thermalArray)
     );
-
+    // Log the client ID of the sensor that sent the data
     console.log('Received sensor data from', data.clientID);
   });
 });
